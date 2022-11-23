c2e5e9a46b941d9e4fcff4454a1440ababde9e7a
HoneyMushrooms <danila8963100@gmail.com>
2022-11-23 07:47:06 +0300

    good morning

--- node/os.js
+++ node/os.js
@@ -1,42 +1,31 @@
 const os = require('os')
 const cluster = require('cluster')
 
-console.log('ос', os.platform()) 
-console.log('архитектура проц', os.arch()) 
-console.log('описание ядер проц', os.cpus())
+//console.log('ос', os.platform()) 
+//console.log('архитектура проц', os.arch()) 
+//console.log('описание ядер проц', os.cpus())
 
 // 2 ядра под ОС
 
 if(cluster.isMaster) {
-    for(let i = 0; i < os.cpus.length; i++){
+    for(let i = 0; i < os.cpus().length - 4; i++){
         cluster.fork() // запуск дочернего процесса
     }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Воркер с pid=${worker.process.pid} умер`)
+        if(code === '200'){
+            cluster.fork()
+        } else {
+            console.log(`Воркер с pid=${worker.process.pid} умер`)
+        }
+    })
+
 } else {
+
+    console.log(`Воркер с pid=${process.pid} запущен`)
+    
     setInterval( () => {
-    console.log(`Воркер с pid=${process.pid} еще работает`)
+        console.log(`Воркер с pid=${process.pid} еще работает`)
     }, 10000)
 }
-
-while (true) {
-    
-}
-
-// if (cluster.isMaster) {
-//     for (let i = 0; i < os.cpus().length - 2; i++) {
-//         cluster.fork()
-//     }
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`Воркер с pid = ${worker.process.pid} умер`)
-//         if(code ===) {
-//             cluster.fork()
-//         } else {
-//             console.log('Воркер умер...')
-//         }
-//     })
-// } else {
-//     console.log(`Воркер с pid= ${process.pid} запущен`)
-//
-//     setInterval(() => {
-//         console.log(`Воркер с pid= ${process.pid} еще работает`)
-//     }, 5000)
-// }
