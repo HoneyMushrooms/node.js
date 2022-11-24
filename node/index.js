815cf6b4aecef8f2a5e178be76090a108dda3a90
Your Name <you@example.com>
2022-11-24 16:46:57 +0300

    24.11.22

--- node/index.js
+++ node/index.js
@@ -1,69 +1,19 @@
-const http = require('http');
-const EventEmitter = require('events');
+const Router = require('../framework/Router.js')
+const Application = require('../framework/Application')
+
 const PORT = process.env.PORT || 5000;
 
-const emmitter = new EventEmitter(); 
-a = 1
-
-class Router{
-    
-    constructor(){
-        this.endpoints = {}
-    }
-
-    request(method = "GET", path, handler){
-        if(!this.endpoints[path]){
-            this.endpoints[path] = {}
-        }
-        const endpoint = this.endpoints[path]
-
-        if(endpoint[method]) {
-            console.log(new Error(`[${method}] по адресу ${path} уже существует`))
-        }
-        endpoint[method] = handler
-
-        emmitter.on(`[${path}]:[${method}]`, (req, res) => {
-            console.log('we there')
-            handler(req, res)
-        })
-    }
-
-    get(path, handler) {
-        this.request ('GET', path, handler);
-    }
-    post(path, handler) {
-        this.request ('POST', path, handler);
-    }
-    put(path, handler) {
-        this.request ('PUT', path, handler);
-    }
-    delete(path, handler) {
-        this.request ('DELETE', path, handler);
-    }
-}
-
+const app = new Application();
 const router = new Router();
 
 router.get('/a', (req, res) => {
     res.end('win')
 })
 
-
-router.get('/posts', (req, res) => {
-    res.end('win')
+router.get('/b', (req, res) => {
+    res.end('win2')
 })
 
-const server = http.createServer((req, res) => {
-    console.log('a = ', a++)
-    const emmitted = emmitter.emit(`[${req.url}]:[${req.method}]`, req, res)
-    
-    if(!emmitted){
-        res.write('ss')
-        res.end(req.url)
-    }
- 
-})
+app.addRouter(router)
 
-server.listen(PORT, () =>{
-    console.log(`server started on PORT ${PORT}`)
-})
+app.listen(PORT, () => { console.log(`server started on PORT ${PORT}`) })
